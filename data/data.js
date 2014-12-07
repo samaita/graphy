2bceb96436757c14371535ebb31521849906d575
Gary Almas <garysamaita@gmail.com>
2014-12-07 15:20:30 +0700

    Read and Write Saved Image
    Read & Write JSON data
    Generate view for each read data
    Reload after successful save

---
+++ data/data.js
@@ -0,0 +1,18 @@
+graphyData([
+{
+"name": "5483f13fb099a",
+"url": "data/5483f13fb099a.png"
+},
+{
+"name": "5483f14303495",
+"url": "data/5483f14303495.png"
+},
+{
+"name": "5483f145e949b",
+"url": "data/5483f145e949b.png"
+}
+,{
+"name" : "54840cad40fff", 
+"url" : "data/54840cad40fff.png"
+}
+]);
--- data/save.php
+++ data/save.php
@@ -1,11 +1,26 @@
 <?php
 	// requires php5
-	define('UPLOAD_DIR', 'data/'); // Ganti ke IP juga bisa
+	define('UPLOAD_DIR', ''); // Saved to this App path only.
 	$img = $_POST['imgBase64'];
 	$img = str_replace('data:image/png;base64,', '', $img);
 	$img = str_replace(' ', '+', $img);
 	$data = base64_decode($img);
-	$file = UPLOAD_DIR . 'images_' . uniqid() . '.png';
-	$success = file_put_contents($file, $data);
-	print $success ? $file : 'Unable to save the file.';
+	$id = uniqid();
+	$name = UPLOAD_DIR . '' . $id . '.png';
+	$success = file_put_contents($name, $data);
+	
+	$getFile = file("data.js"); // Get the file
+	unset($getFile[count($getFile)-1]); // Remove Last Array
+	$getFileS = implode($getFile); // Turn Array to String but in exact condition, not turn it to one line
+	
+$addThis = <<<EOT
+,{
+"name" : "$id", 
+"url" : "data/$name"
+}
+]);
+EOT;
+	
+	file_put_contents("data.js", $getFileS . $addThis); // Join old data and new data
+
 ?>
